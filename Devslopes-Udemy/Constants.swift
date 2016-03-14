2d1cff66e5314b1f60f11d12d0ef5ed382f49947
Emmanuel Darmon <manudarmon@gmail.com>
2016-03-14 16:12:39 +0100

    First commit
    Material design

---
+++ Devslopes-Udemy/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Devslopes-Udemy
+//
+//  Created by Emmanuel Darmon on 11/03/2016.
+//  Copyright © 2016 Emmanuel Darmon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
---
+++ Devslopes-Udemy/MaterialView.swift
@@ -0,0 +1,22 @@
+//
+//  MaterialView.swift
+//  Devslopes-Udemy
+//
+//  Created by Emmanuel Darmon on 11/03/2016.
+//  Copyright © 2016 Emmanuel Darmon. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).CGColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSizeMake(0.0, 2.0)
+
+    }
+    
+}
