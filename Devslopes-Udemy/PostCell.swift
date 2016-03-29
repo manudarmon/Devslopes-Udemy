3223a64871bdfa1b5dd1d58bacc9c7009e10679f
Emmanuel Darmon <manudarmon@gmail.com>
2016-03-29 16:22:41 +0200

    Section 9, lecture 124
    8:18

--- Devslopes-Udemy/PostCell.swift
+++ Devslopes-Udemy/PostCell.swift
@@ -12,6 +12,11 @@ class PostCell: UITableViewCell {
     
     @IBOutlet weak var profileImg: UIImageView!
     @IBOutlet weak var showcaseImg: UIImageView!
+    @IBOutlet weak var descriptionText: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    var post: Post!
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,6 +31,13 @@ class PostCell: UITableViewCell {
     
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    func configureCell(post: Post) {
+        self.post = post
+        
+        self.descriptionText.text = post.postDescription
+        self.likesLbl.text = "\(post.likes)"
     }
 
 }
