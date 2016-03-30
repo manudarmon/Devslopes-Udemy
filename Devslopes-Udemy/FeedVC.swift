dfd6994efd201c2bdcb11b05dc9c9f09b849b465
Emmanuel Darmon <manudarmon@gmail.com>
2016-03-30 15:45:50 +0200

    Section 9, lecture 124
    Done

--- Devslopes-Udemy/FeedVC.swift
+++ Devslopes-Udemy/FeedVC.swift
@@ -13,13 +13,15 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     var posts = [Post]()
-    
+    static var imageCash = NSCache()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        tableView.estimatedRowHeight = 370
         
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
             print(snapshot.value)
@@ -58,10 +60,27 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         let post = posts[indexPath.row]
         
         if let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as? PostCell {
-            cell.configureCell(post)
+            cell.request?.cancel()
+            var img: UIImage?
+            
+            if let url = post.imageURL {
+                img = FeedVC.imageCash.objectForKey(url) as? UIImage
+            }
+            
+            cell.configureCell(post, img: img)
             return cell
         } else {
             return PostCell()
+        }
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let post = posts[indexPath.row]
+        
+        if post.imageURL == nil {
+            return 150
+        } else {
+            return tableView.estimatedRowHeight
         }
     }
 
--- Devslopes-Udemy/PostCell.swift
+++ Devslopes-Udemy/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class PostCell: UITableViewCell {
     
@@ -16,7 +17,7 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var likesLbl: UILabel!
     
     var post: Post!
-    
+    var request: Request?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -33,11 +34,34 @@ class PostCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
     }
     
-    func configureCell(post: Post) {
+    func configureCell(post: Post, img: UIImage?) {
         self.post = post
         
         self.descriptionText.text = post.postDescription
         self.likesLbl.text = "\(post.likes)"
+        
+        if post.imageURL != nil {
+            
+            if img != nil {
+                self.showcaseImg.image = img
+                
+        } else {
+                
+            request = Alamofire.request(.GET, post.imageURL!).validate(contentType: ["image/*"]).response(completionHandler: { request, response, data, err in
+                
+                if err == nil {
+                    let img = UIImage(data: data!)!
+                    self.showcaseImg.image = img
+                    FeedVC.imageCash.setObject(img, forKey: self.post.imageURL!)
+                }
+                
+            })
+        }
+        
+        } else {
+            self.showcaseImg.hidden = true
+        }
+        
     }
 
 }
